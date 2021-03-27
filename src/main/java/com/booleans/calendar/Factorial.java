400732837aa013e29ffb56c4ee59177668b11492
AdityaS1426 <adisurapaneni@gmail.com>
2021-03-26 17:30:05 -0700

    Set up the basic logic for a recursion-based factorial program.

---
+++ src/main/java/com/booleans/calendar/Factorial.java
@@ -0,0 +1,20 @@
+package com.booleans.calendar;
+
+import java.util.Scanner;
+public class Factorial {
+
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+        System.out.println("Enter a number to find its factorial.");
+        int Num = Sc.nextInt();
+        long Factorial = multiplyNumbers(Num);
+        System.out.println("The factorial of " + Num + " is " + Factorial + ".");
+    }
+    public static long multiplyNumbers(int Num)
+    {
+        if (Num >= 1)
+            return Num * multiplyNumbers(Num - 1);
+        else
+            return 1;
+    }
+}
