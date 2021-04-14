e9aeadc0ebef1752e7e47a586476a9ae539dbe8f
YasaswiS <yasaswisingamneni2004@gmail.com>
2021-04-14 00:30:49 -0700

    fixed inheritance

--- src/main/java/com/booleans/calendar/FRQ/Yasaswi/Inheritance.java
+++ src/main/java/com/booleans/calendar/FRQ/Yasaswi/Inheritance.java
@@ -30,32 +30,35 @@ public class Inheritance {
             return "Name: " + name + ", Number of Teachers: " + teachers + ", Number of Students: " + students;
         }
     }
-    public static class DelNorte extends School{
+    public static class DelNorte extends School {
         private int numOfApClasses;
-        public DelNorte (int teachers, int students) {
+
+        public DelNorte(int teachers, int students) {
             super("Del Norte", 2500, 120);
             numOfApClasses = 25;
         }
-    public static class OVMS extends School{
-        private int numOfSciOlyTeams;
-        public OVMS (int teachers, int students) {
-            super("OVMS", 1400, 60);
-            numOfSciOlyTeams = 6;
+
+        public static class OVMS extends School {
+            private int numOfSciOlyTeams;
+
+            public OVMS(int teachers, int students) {
+                super("OVMS", 1400, 60);
+                numOfSciOlyTeams = 6;
+            }
+        }
+
+        public static void main(String[] args) {
+            ArrayList<School> schools = new ArrayList<>();
+
+            School Westview = new School("Westview", 2000, 100);
+            schools.add(Westview);
+
+            School Poway = new School("Poway", 1500, 110);
+            schools.add(Poway);
+
+            schools.forEach((school -> {
+                System.out.println(school.summary());
+            }));
         }
     }
-
-    public static void main(String[] args){
-        ArrayList<School> schools = new ArrayList<>();
-
-        School Westview = new School("Westview", 2000, 100);
-        schools.add(Westview);
-
-        School Poway = new School("Poway", 1500, 110);
-        schools.add(Poway);
-
-        schools.forEach((school -> {
-            System.out.println(school.summary());
-        }));
-    }
-
 }
