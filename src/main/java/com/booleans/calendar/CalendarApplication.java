beae94559c113ecc3619ee8817bca752457368d6
PNatarajan123 <dsanjay@gmail.com>
2021-04-22 20:24:57 -0700

    Added inheritance, working on linking inheritance to the website

--- src/main/java/com/booleans/calendar/CalendarApplication.java
+++ src/main/java/com/booleans/calendar/CalendarApplication.java
@@ -2,9 +2,10 @@ package com.booleans.calendar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CalendarApplication {
+public class CalendarApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CalendarApplication.class, args);
