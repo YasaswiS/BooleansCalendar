481f2843f56b9d2daede94bcf215801325830166
vihan <vihanjay@gmail.com>
2021-04-24 22:26:38 -0700

    Added code to redirect console output to BooleansCalendar/logs/my.log

--- src/main/java/com/booleans/calendar/CalendarApplication.java
+++ src/main/java/com/booleans/calendar/CalendarApplication.java
@@ -4,11 +4,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
 @SpringBootApplication
 public class CalendarApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
+
+
+        //Starting up Spring Application
         SpringApplication.run(CalendarApplication.class, args);
+
+
+
     }
+
 
 }
