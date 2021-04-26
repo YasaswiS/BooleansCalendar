5292d03b1cbf8f0c3c78cd9a6f83243745c034d4
PNatarajan123 <dsanjay@gmail.com>
2021-04-25 22:43:02 -0700

    added time elapsed

--- src/main/java/com/booleans/calendar/MainController.java
+++ src/main/java/com/booleans/calendar/MainController.java
@@ -56,6 +56,7 @@ public class MainController extends SpringBootServletInitializer {
         int factnum = Integer.parseInt(totfact);
 
         model.addAttribute("paths", factorial.fact(factnum));
+        model.addAttribute("facttime", String.valueOf(factorial.getTimeElapsed()));
 
 
         return "labs/Praveen/praveenFactorial";
