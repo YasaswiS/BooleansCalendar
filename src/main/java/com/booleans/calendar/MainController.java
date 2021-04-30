e53a6ceb88c06dd77361ea068a19a4f961505250
PNatarajan123 <dsanjay@gmail.com>
2021-04-29 22:08:10 -0700

    added time elapsed + extra stuff also attempted to deploy inheritance

--- src/main/java/com/booleans/calendar/MainController.java
+++ src/main/java/com/booleans/calendar/MainController.java
@@ -2,6 +2,8 @@ package com.booleans.calendar;
 
 
 import com.booleans.calendar.FRQ.Neil.Inheritance;
+import com.booleans.calendar.FRQ.Vihan.Inheritance.Computer;
+import com.booleans.calendar.FRQ.Vihan.Inheritance.Laptop;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -70,20 +72,39 @@ public class MainController extends SpringBootServletInitializer {
 
         return "labs/Neil/NeilFactorial";
     }
+/*
+    @RequestMapping("/labs/Vihan/Inheritance")
+    public String VihanInheritance(@RequestParam(value = "computerType", required=false, defaultValue="Computer") String computerType,@RequestParam(value = "storage", required=false, defaultValue="256") String storage, @RequestParam(value = "ram", required=false, defaultValue="8") String ram,@RequestParam(value = "name", required=false, defaultValue="Computer") String name,@RequestParam(value = "operatingsystem", required=false, defaultValue="Ubuntu") String operatingsystem,@RequestParam(value = "batteryduration", required=false, defaultValue="8") String batteryduration,Model model) {
 
-    @RequestMapping(value = "/labs/Praveen/Car", method = {RequestMethod.GET, RequestMethod.POST})
-    public String Car(@RequestParam(value = "Gears", required = false, defaultValue = "3") String Gears, @RequestParam(value = "Speed", required = false, defaultValue = "3") String Speed, @RequestParam(value = "Height", required = false, defaultValue = "3") String Height, Model model) {
 
-        CarInheritance car = new CarInheritance();
-        int DaGear = Integer.parseInt(Gears);
-        int DaSpeed = Integer.parseInt(Speed);
-        int DaHeight = Integer.parseInt(Height);
+        System.out.println(computerType);
+        int stor = Integer.parseInt(storage);
+        int ra = Integer.parseInt(ram);
+        int battery = Integer.parseInt(batteryduration);
 
-        model.addAttribute("paths", car);
+        Computer.OperatingSystem o;
+        if (operatingsystem.compareTo("Windows")==0) {
+            o = Computer.OperatingSystem.WINDOWS;
+        } else if (operatingsystem.compareTo("MacOS")==0) {
+            o = Computer.OperatingSystem.MACOS;
+        } else {
+            o = Computer.OperatingSystem.UBUNTU;
+        }
 
-        return "labs/Praveen/PraveenInheritance";
+
+        if (computerType.compareTo("Laptop")==0) {
+            Laptop computer = new Laptop(stor,name,ra,o, battery);
+            model.addAttribute("computerToString",computer.toString());
+
+        } else {
+            Computer computer = new Computer(stor,name,ra,o);
+            model.addAttribute("computerToString",computer.toString());
+        }
+
+
+        return "labs/Vihan/Inheritance";
     }
-
+*/
     @RequestMapping(value = "/labs/Neil/inheritance", method = {RequestMethod.GET, RequestMethod.POST})
     public String Food(@RequestParam(value = "name", required = false, defaultValue = "3") String name, @RequestParam(value = "cost", required = false, defaultValue = "3") String cost, @RequestParam(value = "calories", required = false, defaultValue = "3") String calories, Model model) {
 
