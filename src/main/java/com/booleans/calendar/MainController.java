60566bc66096a626ad61356f812dae441d558d79
PNatarajan123 <dsanjay@gmail.com>
2021-04-30 23:53:26 -0700

    attempted linked list

--- src/main/java/com/booleans/calendar/MainController.java
+++ src/main/java/com/booleans/calendar/MainController.java
@@ -136,5 +136,20 @@ public class MainController extends SpringBootServletInitializer {
         return "labs/Yasaswi/YasaswiLinkedList";
     }
 
+    @RequestMapping(value = "/labs/Praveen/LinkedList", method = {RequestMethod.GET, RequestMethod.POST})
+    public String LinkedListPraveen(@RequestParam(value = "add", required = false, defaultValue = "7") String add, Model model) {
 
+        LinkedList list = new LinkedList(new int[]{9, 1, 8, 5, 2,3});
+
+        int val = Integer.parseInt(add);
+
+        list.addHead(val);
+
+        model.addAttribute("linkedlist1", list.traverse().get(0));
+        model.addAttribute("linkedlist2", list.traverse().get(1));
+        model.addAttribute("linkedlist3", list.traverse().get(list.traverse().size()-1));
+        model.addAttribute("linkedlist4", list.traverse());
+
+        return "labs/Praveen/PraveenLinkedList";
+    }
 }
