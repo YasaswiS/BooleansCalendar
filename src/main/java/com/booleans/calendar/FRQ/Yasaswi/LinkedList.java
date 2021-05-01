20c95c3d6c5092f3391b25f395ae1c46f1dfb6fc
YasaswiS <yasaswisingamneni2004@gmail.com>
2021-05-01 00:13:39 -0700

    fixed addHeads

--- src/main/java/com/booleans/calendar/FRQ/Yasaswi/LinkedList.java
+++ src/main/java/com/booleans/calendar/FRQ/Yasaswi/LinkedList.java
@@ -25,19 +25,19 @@ public class LinkedList{
 
     public void addHead(int val){
         int[] vals = new int[nodes.size()];
-        for (int i = 0; i < nodes.size(); i++){
-            vals[i] = nodes.get(i).value;
-        }
+//        for (int i = 0; i < nodes.size(); i++){
+//            vals[i] = nodes.get(i).value;
+//        }
         this.head = new Node(val);
         //System.out.println(head.value);
-        ArrayList<Node> nodes = new ArrayList<Node>();
-        nodes.add(head);
+        //ArrayList<Node> nodes = new ArrayList<Node>();
+        nodes.add(0, head);
 
 
-        for (int i = 0; i < vals.length; i++){
-
-            nodes.add(new Node(vals[i]));
-        }
+//        for (int i = 0; i < vals.length; i++){
+//
+//            nodes.add(new Node(vals[i]));
+//        }
         for (int i = 0; i < nodes.size()-1; i++){
             nodes.get(i).next = nodes.get(i+1);
         }
@@ -104,7 +104,7 @@ public class LinkedList{
 
         //System.out.println(linkedList.head.value);
         //System.out.println(linkedList.head.next.value);
-        //linkedList.addHead(6);
+        linkedList.addHead(6);
         System.out.println(linkedList.traverse());
         linkedList.addTail(8);
         System.out.println(linkedList.traverse());
