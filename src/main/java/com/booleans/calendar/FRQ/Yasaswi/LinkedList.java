af4ff38fc85ce4aeabad31d720df6877776ddbc6
YasaswiS <49824724+YasaswiS@users.noreply.github.com>
2021-04-30 22:47:42 -0700

    Update LinkedList.java

--- src/main/java/com/booleans/calendar/FRQ/Yasaswi/LinkedList.java
+++ src/main/java/com/booleans/calendar/FRQ/Yasaswi/LinkedList.java
@@ -4,12 +4,12 @@ import java.util.ArrayList;
 
 public class LinkedList{
 
-    Node head;
-    ArrayList<Node> nodes;
+    public Node head;
+    public ArrayList<Node> nodes;
 
     public LinkedList(int[] vals){
         this.head = new Node(vals[0]);
-        System.out.println(head.value);
+        //System.out.println(head.value);
         this.nodes = new ArrayList<Node>();
         nodes.add(head);
 
@@ -25,19 +25,28 @@ public class LinkedList{
 
     public void addHead(int val){
         int[] vals = new int[nodes.size()];
-        this.head = new Node(vals[0]);
-        System.out.println(head.value);
+        for (int i = 0; i < nodes.size(); i++){
+            vals[i] = nodes.get(i).value;
+        }
+        this.head = new Node(val);
+        //System.out.println(head.value);
         ArrayList<Node> nodes = new ArrayList<Node>();
         nodes.add(head);
 
-//        for (int i = 1; i < vals.length; i++){
-//            int val = vals[i];
-//            nodes.add(new Node(val));
-//        }
-//        for (int i = 0; i < nodes.size() - 1; i++){
-//            nodes.get(i).next = nodes.get(i+1);
-//        }
 
+        for (int i = 0; i < vals.length; i++){
+
+            nodes.add(new Node(vals[i]));
+        }
+        for (int i = 0; i < nodes.size() - 1; i++){
+            nodes.get(i).next = nodes.get(i+1);
+        }
+
+    }
+
+    public void addTail(int val){
+        nodes.add(new Node(val));
+        nodes.get(nodes.size()-1).next = nodes.get(nodes.size());
     }
 
     // create an object of Node class
@@ -89,8 +98,10 @@ public class LinkedList{
 
         System.out.println(linkedList.traverse());
 
-        System.out.println(linkedList.head.value);
-        System.out.println(linkedList.head.next.value);
+        //System.out.println(linkedList.head.value);
+        //System.out.println(linkedList.head.next.value);
+        linkedList.addHead(6);
+        System.out.println(linkedList.traverse());
 
     }
 }
