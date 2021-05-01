b0d87e27d54ff9a062255b22bcab543753ef0472
YasaswiS <49824724+YasaswiS@users.noreply.github.com>
2021-04-30 23:03:23 -0700

    Update MainController.java

--- src/main/java/com/booleans/calendar/MainController.java
+++ src/main/java/com/booleans/calendar/MainController.java
@@ -119,19 +119,22 @@ public class MainController extends SpringBootServletInitializer {
         return "labs/Neil/NeilInheritance";
     }
 
-    @RequestMapping(value = "/labs/Yasaswi/LinkedList", method = {RequestMethod.GET, RequestMethod.POST})
-    public String LinkedList(@RequestParam(value = "name", required = false, defaultValue = "1") String name, Model model) {
+       @RequestMapping(value = "/labs/Yasaswi/LinkedList", method = {RequestMethod.GET, RequestMethod.POST})
+    public String LinkedList(@RequestParam(value = "add", required = false, defaultValue = "7") String add, Model model) {
 
-        LinkedList list = new LinkedList(new int[]{1,2,3});
+        LinkedList list = new LinkedList(new int[]{9, 1, 8, 5, 2,3});
+
+        int val = Integer.parseInt(add);
+
+        list.addHead(val);
 
         model.addAttribute("linkedlist1", list.traverse().get(0));
         model.addAttribute("linkedlist2", list.traverse().get(1));
         model.addAttribute("linkedlist3", list.traverse().get(list.traverse().size()-1));
+        model.addAttribute("linkedlist4", list.traverse());
 
         return "labs/Yasaswi/YasaswiLinkedList";
     }
-
-
 
 
 }
